dba2db3a7342aa7be91bf7b54721e8f016492826
Pablo Vazquez <pvazquez@vandldigital.com>
2020-04-29 10:13:33 -0400

    order model

---
+++ models/Order.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    name: {type: String},
+    email: {type: String},
+    phone: {type: String}
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
